a479ada0baf018884ca0f8727d639cdcf023c89e
Brian Salchert <brisalchert@gmail.com>
2024-10-09 12:00:01 -0400

    Add constructor, Node class, and isEmpty()

--- Module1/Bag.java
+++ Module1/Bag.java
@@ -8,7 +8,42 @@
 //    should return false.
 //  - "int count(T item)": This method should count the occurrences of an item in the bag and return the total
 //    number of occurrences.
+//  - "boolean isEmpty()": This method should return true if the bag is empty; otherwise, it returns false.
 //----------------------------------------------------------------------------------------------------------------------
 
-public class Bag {
+public class Bag<T> {
+    // Node for the first item in the bag
+    private Node head = null;
+    // Integer for the size of the bag
+    private int size;
+
+    /**
+     * Constructor: Initializes an empty bag with a size of 0
+     */
+    public Bag() {
+        this.size = 0;
+    }
+
+    /**
+     * Returns a boolean corresponding to whether or not the bag is empty
+     * @return true if the bag is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Node class for Linked List implementation of the Bag ADT
+    private class Node {
+        private T item;
+        private Node next;
+
+        /**
+         * Constructor: Initializes a new Node with an item of type T and a next node of null
+         * @param item the item to store in the Node
+         */
+        private Node(T item) {
+            this.item = item;
+            this.next = null;
+        }
+    }
 }
