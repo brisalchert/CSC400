e5f9ec87c15a690a1c3100aa58ab3f6a857f5425
Brian Salchert <brisalchert@gmail.com>
2024-10-09 11:42:44 -0400

    Add Bag.java with header comment

---
+++ Module1/Bag.java
@@ -0,0 +1,14 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Bag.java
+//
+//  Implementation of the Java Bag data structure, A.K.A a multiset. Includes the following methods:
+//  - "void add(T item)": This method should add an item of type T to the bag.
+//  - "void remove(T item)": This method should remove one occurrence of the item from the bag, if it exists.
+//  - "boolean contains(T item)": This method should return true if the item exists in the bag; otherwise, it
+//    should return false.
+//  - "int count(T item)": This method should count the occurrences of an item in the bag and return the total
+//    number of occurrences.
+//----------------------------------------------------------------------------------------------------------------------
+
+public class Bag {
+}
