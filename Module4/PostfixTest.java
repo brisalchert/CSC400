0a507f26ae59ff2c77207e6e7a649df19c464e05
Brian Salchert <brisalchert@gmail.com>
2024-10-30 11:45:24 -0400

    Add file reading and evaluation testing

--- Module4/PostfixTest.java
+++ Module4/PostfixTest.java
@@ -5,18 +5,41 @@
 //  inputs and displays the proper results.
 //----------------------------------------------------------------------------------------------------------------------
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class PostfixTest {
     public static void main(String[] args) {
+        // Initialize the calculator
         PostfixCalculator calculator = new PostfixCalculator();
 
-        String expression = "42 5 + 7 / -3 * 5 %";
-        try {
-            System.out.println(calculator.evaluatePostfix(expression));
+        // Initialize a Scanner (using try-with-resources) to read input from a file line-by-line
+        try (Scanner scanner = new Scanner(new File("PostfixExpressions.txt"))) {
+            int counter = 1;
+
+            // Evaluate input for each line in the file
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                // Evaluate the postfix expression
+                System.out.print("Result " + counter + ": ");
+
+                try {
+                    // Print the expression result for valid expressions
+                    System.out.println(calculator.evaluatePostfix(line));
+                }
+                catch (InputMismatchException e) {
+                    // Print error message for invalid expressions
+                    System.out.println(e.getMessage());
+                }
+
+                counter++;
+            }
         }
-        catch (InputMismatchException e) {
-            System.out.println(e.getMessage());
+        catch (FileNotFoundException error) {
+            System.out.println("Error reading file");
         }
     }
 }
