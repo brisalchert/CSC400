da940056e6d2bc5882179c8ef8ca744183dd4ad4
Brian Salchert <brisalchert@gmail.com>
2024-10-30 10:55:26 -0400

    Add postfix evaluation logic

--- Module4/PostfixCalculator.java
+++ Module4/PostfixCalculator.java
@@ -16,10 +16,13 @@
 //  calculator can distinguish between single-digit and multi-digit operands.
 //----------------------------------------------------------------------------------------------------------------------
 
+import java.util.EmptyStackException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class PostfixCalculator {
-    private Stack<Integer> numStack;
+    private final Stack<Integer> numStack;
 
     /**
      * Constructor: Initializes a PostfixCalculator with a numStack
@@ -28,7 +31,88 @@ public class PostfixCalculator {
         numStack = new Stack<>();
     }
 
-    public int evaluatePostfix(String expression) {
-        return 0;
+    /**
+     * Evaluates a postfix expression of integer operands and operators and returns the result
+     * @param expression the postfix expression
+     * @return the result as an integer
+     * @throws InputMismatchException if the expression is invalid
+     */
+    public int evaluatePostfix(String expression) throws InputMismatchException {
+        int result;
+        // Initialize a scanner for parsing input
+        Scanner scan  = new Scanner(expression);
+
+        // Initialize String of valid operators
+        String operators = "+-*/%";
+
+        // Parse input until it is exhausted
+        while (scan.hasNext()) {
+            // Parse the next token
+            String token = scan.next();
+
+            // If the token is an operator, perform the operation. Otherwise,
+            // the token is an operand, so push it to numStack
+            if (operators.contains(token)) {
+                // Attempt the operation, throwing an error if there are not
+                // enough operands
+                try {
+                    performOperation(token);
+                }
+                catch (EmptyStackException error) {
+                    throw new InputMismatchException("Invalid postfix expression");
+                }
+            } else {
+                // Attempt to push the operand to numStack, throwing an error
+                // if the operand is invalid
+                try {
+                    numStack.push(Integer.parseInt(token));
+                }
+                catch (NumberFormatException error) {
+                    throw new InputMismatchException("Invalid postfix expression");
+                }
+            }
+        }
+
+        // Pop the resulting value off of numStack
+        result = numStack.pop();
+
+        // If there are no more operands, return the result. Otherwise, the
+        // expression is invalid, so throw an error
+        if (numStack.isEmpty()) {
+            return result;
+        } else {
+            throw new InputMismatchException("Invalid postfix expression");
+        }
+    }
+
+    /**
+     * Helper method for performing a postfix expression operation on the last two elements in numStack
+     * @param operation the operation to perform
+     * @throws EmptyStackException if there are not enough operands for the operation
+     */
+    private void performOperation(String operation) throws EmptyStackException {
+        // Retrieve the last two operands from numStack, being
+        // careful to preserve order for division operations
+        int num2 = numStack.pop();
+        int num1 = numStack.pop();
+
+        // Select the correct operation
+        switch (operation) {
+            case "+":
+                numStack.push(num1 + num2);
+                break;
+            case "-":
+                numStack.push(num1 - num2);
+                break;
+            case "*":
+                numStack.push(num1 * num2);
+                break;
+            case "/":
+                numStack.push(num1 / num2);
+                break;
+            case "%":
+                numStack.push(num1 % num2);
+                break;
+        }
     }
 }
