d4969684ec3df4b35ebc7f47cf79a842a41695e3
Brian Salchert <brisalchert@gmail.com>
2024-10-30 10:17:44 -0400

    Create PostfixCalculator.java

---
+++ Module4/PostfixCalculator.java
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  PostfixCalculator.java
+//
+//  A calculator for evaluating postfix expressions. Postfix expressions affix binary operators after the operands
+//  rather than between them. For example, x + y becomes xy+. The benefit of this is that parentheses are not
+//  necessary in postfix expressions, assuming the expression is written correctly. The calculator supports the
+//  following operations:
+//
+//  - Addition (+)
+//  - Subtraction (-)
+//  - Multiplication (*)
+//  - Division (/)
+//  - Modulo (%)
+//
+//  The calculator takes input where all input elements are delimited by spaces. This is to ensure that the
+//  calculator can distinguish between single-digit and multi-digit operands.
+//----------------------------------------------------------------------------------------------------------------------
+
+public class PostfixCalculator {
+
+
+    public int evaluatePostfix(String expression) {
+        return 0;
+    }
+}
