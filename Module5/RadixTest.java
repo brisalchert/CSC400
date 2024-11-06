79aecd84b0e6abec6b6735ccf15c3bbad7d3594f
Brian Salchert <brisalchert@gmail.com>
2024-11-06 12:38:40 -0500

    Add testing code and output

--- Module5/RadixTest.java
+++ Module5/RadixTest.java
@@ -5,8 +5,20 @@
 //  radix sort.
 //----------------------------------------------------------------------------------------------------------------------
 
+import java.util.Arrays;
+
 public class RadixTest {
     public static void main(String[] args) {
-        
+        // Initialize input array
+        int[] input = {783, 99, 472, 182, 264, 543, 356, 295, 692, 491, 94};
+
+        // Print array before sorting
+        System.out.println("Array before sorting: " + Arrays.toString(input));
+
+        // Perform radix sort
+        IntegerRadixSort.radixSort(input);
+
+        // Print the array after sorting
+        System.out.println("Array after sorting: " + Arrays.toString(input));
     }
 }
