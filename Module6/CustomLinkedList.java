29f5325498d6c13964ccb5c271c43182cae6b7e8
Brian Salchert <brisalchert@gmail.com>
2024-11-13 12:19:19 -0500

    Add Javadoc comments

--- Module6/CustomLinkedList.java
+++ Module6/CustomLinkedList.java
@@ -8,16 +8,27 @@ public class CustomLinkedList {
     private Node head;
     private int size;
 
+    /**
+     * Constructor: Creates a new empty linked list
+     */
     public CustomLinkedList() {
         head = null;
         size = 0;
     }
 
+    /**
+     * Constructor: Creates a new linked list with data at its head
+     * @param data the data to store in the head node
+     */
     public CustomLinkedList(int data) {
         head = new Node(data);
         size = 1;
     }
 
+    /**
+     * Inserts a new integer at the end of the list
+     * @param data the integer to insert
+     */
     public void insert(int data) {
         Node newNode = new Node(data);
 
@@ -39,6 +50,10 @@ public class CustomLinkedList {
         size++;
     }
 
+    /**
+     * Deletes the first occurrence of data in the list if found
+     * @param data the integer to delete
+     */
     public void delete(int data) {
         // Return if the list is empty
         if (head == null) {
@@ -70,6 +85,10 @@ public class CustomLinkedList {
         }
     }
 
+    /**
+     * Returns the number of elements in the list
+     * @return the size of the list
+     */
     public int size() {
         return size;
     }
@@ -78,24 +97,42 @@ public class CustomLinkedList {
         return new LinkedListIterator();
     }
 
+    /**
+     * Inner class for linked list nodes
+     */
     private class Node {
         int data;
         Node next;
 
+        /**
+         * Creates a new node with data as its data
+         * @param data the data of the node
+         */
         Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
 
+    /**
+     * Inner class for an iterator over the integers in the linked list
+     */
     private class LinkedListIterator implements Iterator<Integer> {
         private Node current = head;
 
+        /**
+         * Checks if the iterator has a next integer
+         * @return true if there is a next integer
+         */
         @Override
         public boolean hasNext() {
             return current != null;
         }
 
+        /**
+         * Gets the next integer in the iterator and increments the iterator
+         * @return the next integer in the iterator
+         */
         @Override
         public Integer next() {
             if (!hasNext()) {
